81682b13c6696f6d3e6700c24821342a9400ef29
Prash9 <prashantkamath94@gmail.com>
2021-04-05 22:17:49 +0530

    minor

--- bhavcopy_app/scheduler.py
+++ bhavcopy_app/scheduler.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime,timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 from .equity import Equity
 
@@ -9,6 +9,6 @@ def start():
     scheduler = BackgroundScheduler()
     # scheduler.add_job(test, 'interval', seconds=10)
     # scheduler.add_job(Equity().update_bhavcopy, 'cron',[datetime(2021,4,1)],minute='*/2')
-    scheduler.add_job(Equity().update_bhavcopy, 'cron',[datetime.now()], day_of_week='mon-fri', hour='4',minute='30')
+    scheduler.add_job(Equity().update_bhavcopy, 'cron',[datetime.now()-timedelta(days=1)], day_of_week='mon-fri', hour='4',minute='30')
     scheduler.add_job(Equity().update_bhavcopy, 'cron',[datetime.now()],day_of_week='mon-fri', hour='12',minute='40')
     scheduler.start()
